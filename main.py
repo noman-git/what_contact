48c180256102e2c5147091ea583288e9ac6baa37
= <=>
2023-09-07 10:05:54 +0500

    Added docker support

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from logic import check_contacts_on_whatsapp
 from utils import setup_browser
 
 def main():
-    browser, wait = setup_browser(30)
-    
-    # Wait for the QR code to be scanned
-    input("Press Enter after scanning the QR code...")
+    browser, wait = setup_browser(40)
 
     contacts_to_check = ['+923227942883', '+923227942881', '+923227942885']
     results = check_contacts_on_whatsapp(wait, contacts_to_check)
---
+++ reademe.md
@@ -0,0 +1,4 @@
+## How to run
+```docker compose build```
+
+```docker compose up```
